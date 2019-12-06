e5c3aa2887472bb4688e18c286265a49b6291341
esteth <adam.copp@gmail.com>
2019-12-06 13:54:14 +0000

    Solve 5-2

--- src/computer.rs
+++ src/computer.rs
@@ -22,6 +22,10 @@ enum Instruction {
     Mult(Parameter, Parameter, Parameter),
     Input(Parameter),
     Output(Parameter),
+    JumpIfTrue(Parameter, Parameter),
+    JumpIfFalse(Parameter, Parameter),
+    LessThan(Parameter, Parameter, Parameter),
+    Equals(Parameter, Parameter, Parameter),
     Exit,
 }
 
@@ -40,7 +44,7 @@ impl Parameter {
                 } else {
                     Ok(Parameter::Position(value as usize))
                 }
-            },
+            }
             1 => Ok(Parameter::Immediate(value)),
             _ => Err(SimpleError::new("Invalid mode bit")),
         }
@@ -105,6 +109,20 @@ impl Computer<'_> {
                     Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?),
                 4 => Instruction::Output(
                     Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?),
+                5 => Instruction::JumpIfTrue(
+                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?),
+                6 => Instruction::JumpIfFalse(
+                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?),
+                7 => Instruction::LessThan(
+                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?,
+                    Parameter::new((self.memory[self.ip + 3]) as i32, modes[2])?),
+                8 => Instruction::Equals(
+                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?,
+                    Parameter::new((self.memory[self.ip + 3]) as i32, modes[2])?),
                 99 => Instruction::Exit,
                 _ => return Err(Box::new(ComputeError {}))
             };
@@ -122,7 +140,6 @@ impl Computer<'_> {
                 self.ip += 4;
             }
             Instruction::Input(Parameter::Position(dest)) => {
-                write!(self.output, "Please provide input: ");
                 self.output.flush();
                 let mut input = String::new();
                 self.input.read_line(&mut input).unwrap();
@@ -134,6 +151,36 @@ impl Computer<'_> {
                 let val = self.resolve(x);
                 writeln!(self.output, "{}", val);
                 self.ip += 2;
+            }
+            Instruction::JumpIfTrue(test, loc) => {
+                if self.resolve(test) != 0 {
+                    self.ip = self.resolve(loc) as usize;
+                } else {
+                    self.ip += 3;
+                }
+            }
+            Instruction::JumpIfFalse(test, loc) => {
+                if self.resolve(test) == 0 {
+                    self.ip = self.resolve(loc) as usize;
+                } else {
+                    self.ip += 3;
+                }
+            }
+            Instruction::LessThan(x, y, Parameter::Position(dest)) => {
+                if self.resolve(x) < self.resolve(y) {
+                    self.memory[dest] = 1;
+                } else {
+                    self.memory[dest] = 0;
+                }
+                self.ip += 4
+            }
+            Instruction::Equals(x, y, Parameter::Position(dest)) => {
+                if self.resolve(x) == self.resolve(y) {
+                    self.memory[dest] = 1;
+                } else {
+                    self.memory[dest] = 0;
+                }
+                self.ip += 4
             }
             Instruction::Exit => return Ok(true),
             _ => {
@@ -239,5 +286,50 @@ mod tests {
         let mut c = Computer::new(vec![101, 2, 1, 0, 99], &mut input, &mut output);
         c.run_to_completion().unwrap();
         assert_eq!(c.memory, vec![3, 2, 1, 0, 99]);
+    }
+
+    #[test]
+    fn equals() {
+        let mut input = Cursor::new("8\n");
+        let mut output = Vec::new();
+        let mut c = Computer::new(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], &mut input, &mut output);
+        c.run_to_completion().unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "1\n")
+    }
+
+    #[test]
+    fn equals_immediate() {
+        let mut input = Cursor::new("8\n");
+        let mut output = Vec::new();
+        let mut c = Computer::new(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99], &mut input, &mut output);
+        c.run_to_completion().unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "1\n")
+    }
+
+    #[test]
+    fn less_than() {
+        let mut input = Cursor::new("8\n");
+        let mut output = Vec::new();
+        let mut c = Computer::new(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], &mut input, &mut output);
+        c.run_to_completion().unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "0\n")
+    }
+
+    #[test]
+    fn less_than_immediate() {
+        let mut input = Cursor::new("8\n");
+        let mut output = Vec::new();
+        let mut c = Computer::new(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99], &mut input, &mut output);
+        c.run_to_completion().unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "0\n")
+    }
+
+    #[test]
+    fn jump() {
+        let mut input = Cursor::new("40\n");
+        let mut output = Vec::new();
+        let mut c = Computer::new(vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], &mut input, &mut output);
+        c.run_to_completion().unwrap();
+        assert_eq!(String::from_utf8(output).unwrap(), "1\n")
     }
 }
