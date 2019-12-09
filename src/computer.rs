22e86eab84a4a0fa05f9fb61bb1bd23cbd50f6c0
esteth <adam.copp@gmail.com>
2019-12-09 15:00:49 +0000

    Solve 9-1. Kinda ugly, need to refactor since we'll def be using this again.

--- src/computer.rs
+++ src/computer.rs
@@ -1,11 +1,8 @@
 use std::result::Result;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
-use std::io::{BufRead, Write};
-use std::path::Iter;
 use simple_error::SimpleError;
 use crossbeam_channel::{Sender, Receiver};
-use std::thread;
 
 #[derive(Copy, Clone, Debug)]
 struct ComputeError {}
@@ -28,17 +25,19 @@ enum Instruction {
     JumpIfFalse(Parameter, Parameter),
     LessThan(Parameter, Parameter, Parameter),
     Equals(Parameter, Parameter, Parameter),
+    ModifyRelativeBase(Parameter),
     Exit,
 }
 
 #[derive(Debug)]
 enum Parameter {
-    Immediate(i32),
+    Immediate(i64),
     Position(usize),
+    Relative(isize),
 }
 
 impl Parameter {
-    fn new(value: i32, mode_bit: i32) -> Result<Parameter, SimpleError> {
+    fn new(value: i64, mode_bit: i32) -> Result<Parameter, SimpleError> {
         match mode_bit {
             0 => {
                 if value < 0 {
@@ -48,6 +47,7 @@ impl Parameter {
                 }
             }
             1 => Ok(Parameter::Immediate(value)),
+            2 => Ok(Parameter::Relative(value as isize)),
             _ => Err(SimpleError::new("Invalid mode bit")),
         }
     }
@@ -55,31 +55,35 @@ impl Parameter {
 
 pub struct Computer {
     ip: usize,
-    memory: Vec<i32>,
-    input: Receiver<i32>,
-    output: Sender<i32>,
+    rb: isize,
+    memory: Vec<i64>,
+    input: Receiver<i64>,
+    output: Sender<i64>,
 }
 
 impl std::fmt::Debug for Computer {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "Computer{{ip={:?}, memory={:?}}}", self.ip, self.memory)
+        write!(f, "Computer{{ip={:?}, memory_at_ip={:?}}}", self.ip, self.memory[self.ip])
     }
 }
 
 impl Computer {
     pub fn new(
-        memory: Vec<i32>,
-        input: Receiver<i32>,
-        output: Sender<i32>) -> Computer {
+        memory: Vec<i64>,
+        input: Receiver<i64>,
+        output: Sender<i64>) -> Computer {
+        let mut large_memory = vec![0; 1_000_000_000];
+        large_memory[..memory.len()].copy_from_slice(&memory);
         Computer {
             ip: 0,
-            memory,
+            rb: 0,
+            memory: large_memory,
             input,
             output,
         }
     }
 
-    pub fn run_to_completion(&mut self) -> Result<i32, Box<dyn Error>> {
+    pub fn run_to_completion(&mut self) -> Result<i64, Box<dyn Error>> {
         loop {
             if self.step()? {
                 break;
@@ -93,55 +97,61 @@ impl Computer {
         let mut m = self.memory[self.ip as usize] / 100;
         let mut modes = Vec::new();
         for _ in 0..3 {
-            modes.push(m % 10);
+            modes.push((m % 10) as i32);
             m /= 10;
         }
 
         let instruction =
             match opcode {
                 1 => Instruction::Add(
-                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
-                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?,
-                    Parameter::new((self.memory[self.ip + 3]) as i32, modes[2])?),
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i64, modes[1])?,
+                    Parameter::new((self.memory[self.ip + 3]) as i64, modes[2])?),
                 2 => Instruction::Mult(
-                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
-                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?,
-                    Parameter::new((self.memory[self.ip + 3]) as i32, modes[2])?),
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i64, modes[1])?,
+                    Parameter::new((self.memory[self.ip + 3]) as i64, modes[2])?),
                 3 => Instruction::Input(
-                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?),
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?),
                 4 => Instruction::Output(
-                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?),
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?),
                 5 => Instruction::JumpIfTrue(
-                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
-                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?),
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i64, modes[1])?),
                 6 => Instruction::JumpIfFalse(
-                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
-                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?),
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i64, modes[1])?),
                 7 => Instruction::LessThan(
-                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
-                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?,
-                    Parameter::new((self.memory[self.ip + 3]) as i32, modes[2])?),
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i64, modes[1])?,
+                    Parameter::new((self.memory[self.ip + 3]) as i64, modes[2])?),
                 8 => Instruction::Equals(
-                    Parameter::new((self.memory[self.ip + 1]) as i32, modes[0])?,
-                    Parameter::new((self.memory[self.ip + 2]) as i32, modes[1])?,
-                    Parameter::new((self.memory[self.ip + 3]) as i32, modes[2])?),
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?,
+                    Parameter::new((self.memory[self.ip + 2]) as i64, modes[1])?,
+                    Parameter::new((self.memory[self.ip + 3]) as i64, modes[2])?),
+                9 => Instruction::ModifyRelativeBase(
+                    Parameter::new((self.memory[self.ip + 1]) as i64, modes[0])?),
                 99 => Instruction::Exit,
                 _ => return Err(Box::new(ComputeError {}))
             };
+        println!("{:?}", instruction);
         match instruction {
-            Instruction::Add(x, y, Parameter::Position(dest)) => {
+            Instruction::Add(x, y, dest) => {
                 let x = self.resolve(x);
                 let y = self.resolve(y);
+                let dest = self.resolve_save(dest) as usize;
                 self.memory[dest] = x + y;
                 self.ip += 4;
             }
-            Instruction::Mult(x, y, Parameter::Position(dest)) => {
+            Instruction::Mult(x, y, dest) => {
                 let x = self.resolve(x);
                 let y = self.resolve(y);
+                let dest = self.resolve_save(dest) as usize;
                 self.memory[dest] = x * y;
                 self.ip += 4;
             }
-            Instruction::Input(Parameter::Position(dest)) => {
+            Instruction::Input(dest) => {
+                let dest = self.resolve_save(dest) as usize;
                 let val = self.input.recv()?;
                 self.memory[dest] = val;
                 self.ip += 2;
@@ -165,7 +175,8 @@ impl Computer {
                     self.ip += 3;
                 }
             }
-            Instruction::LessThan(x, y, Parameter::Position(dest)) => {
+            Instruction::LessThan(x, y, dest) => {
+                let dest = self.resolve_save(dest) as usize;
                 if self.resolve(x) < self.resolve(y) {
                     self.memory[dest] = 1;
                 } else {
@@ -173,7 +184,8 @@ impl Computer {
                 }
                 self.ip += 4
             }
-            Instruction::Equals(x, y, Parameter::Position(dest)) => {
+            Instruction::Equals(x, y, dest) => {
+                let dest = self.resolve_save(dest) as usize;
                 if self.resolve(x) == self.resolve(y) {
                     self.memory[dest] = 1;
                 } else {
@@ -181,9 +193,14 @@ impl Computer {
                 }
                 self.ip += 4
             }
+            Instruction::ModifyRelativeBase(x) => {
+                self.rb += self.resolve(x) as isize;
+                self.ip += 2;
+                println!("new rb: {}", self.rb);
+            }
             Instruction::Exit => return Ok(true),
             _ => {
-                eprintln!("error: pc={}, memory={:?}", self.ip, self.memory);
+                eprintln!("error: {:?}", self);
                 return Err(Box::new(ComputeError {}));
             }
         }
@@ -191,10 +208,24 @@ impl Computer {
         Ok(false)
     }
 
-    fn resolve(&self, x: Parameter) -> i32 {
+    fn resolve(&self, x: Parameter) -> i64 {
         match x {
             Parameter::Immediate(x) => x,
             Parameter::Position(x) => self.memory[x],
+            Parameter::Relative(x) => {
+                println!("resolve relative to : {} = {}", (self.rb + x), self.memory[(self.rb + x) as usize]);
+                self.memory[(self.rb + x) as usize]
+            }
+        }
+    }
+
+    fn resolve_save(&self, x: Parameter) -> i64 {
+        match x {
+            Parameter::Immediate(x) =>  panic!("wat"),
+            Parameter::Position(x) => x as i64,
+            Parameter::Relative(x) => {
+                (self.rb + x) as i64
+            }
         }
     }
 }
@@ -202,7 +233,6 @@ impl Computer {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::Cursor;
 
     #[test]
     fn first_example() {
@@ -217,10 +247,10 @@ mod tests {
                 rx1,
                 tx2);
         c.run_to_completion().unwrap();
-        assert_eq!(c.memory, vec![3500, 9, 10, 70,
-                                  2, 3, 11, 0,
-                                  99,
-                                  30, 40, 50]);
+        assert_eq!(c.memory[..12], vec![3500, 9, 10, 70,
+                                        2, 3, 11, 0,
+                                        99,
+                                        30, 40, 50][..]);
     }
 
     #[test]
@@ -228,7 +258,7 @@ mod tests {
         let (tx, rx) = crossbeam_channel::unbounded();
         let mut c = Computer::new(vec![1, 0, 0, 0, 99], rx, tx);
         c.run_to_completion().unwrap();
-        assert_eq!(c.memory, vec![2, 0, 0, 0, 99]);
+        assert_eq!(c.memory[..5], vec![2, 0, 0, 0, 99][..]);
     }
 
     #[test]
@@ -236,7 +266,7 @@ mod tests {
         let (tx, rx) = crossbeam_channel::unbounded();
         let mut c = Computer::new(vec![2, 3, 0, 3, 99], rx, tx);
         c.run_to_completion().unwrap();
-        assert_eq!(c.memory, vec![2, 3, 0, 6, 99]);
+        assert_eq!(c.memory[..5], vec![2, 3, 0, 6, 99][..]);
     }
 
     #[test]
@@ -244,7 +274,7 @@ mod tests {
         let (tx, rx) = crossbeam_channel::unbounded();
         let mut c = Computer::new(vec![2, 4, 4, 5, 99, 0], rx, tx);
         c.run_to_completion().unwrap();
-        assert_eq!(c.memory, vec![2, 4, 4, 5, 99, 9801]);
+        assert_eq!(c.memory[..6], vec![2, 4, 4, 5, 99, 9801][..]);
     }
 
     #[test]
@@ -252,7 +282,7 @@ mod tests {
         let (tx, rx) = crossbeam_channel::unbounded();
         let mut c = Computer::new(vec![1, 1, 1, 4, 99, 5, 6, 0, 99], rx, tx);
         c.run_to_completion().unwrap();
-        assert_eq!(c.memory, vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
+        assert_eq!(c.memory[..9], vec![30, 1, 1, 4, 2, 5, 6, 0, 99][..]);
     }
 
     #[test]
@@ -261,7 +291,7 @@ mod tests {
         tx.send(10).unwrap();
         let mut c = Computer::new(vec![3, 0, 99], rx, tx);
         c.run_to_completion().unwrap();
-        assert_eq!(c.memory, vec![10, 0, 99]);
+        assert_eq!(c.memory[..3], vec![10, 0, 99][..]);
     }
 
     #[test]
@@ -270,7 +300,7 @@ mod tests {
         let (tx2, rx2) = crossbeam_channel::unbounded();
         let mut c = Computer::new(vec![4, 0, 99], rx1, tx2);
         c.run_to_completion().unwrap();
-        assert_eq!(c.memory, vec![4, 0, 99]);
+        assert_eq!(c.memory[..3], vec![4, 0, 99][..]);
         assert_eq!(rx2.recv().unwrap(), 4)
     }
 
@@ -331,5 +361,32 @@ mod tests {
         let mut c = Computer::new(vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], rx1, tx2);
         c.run_to_completion().unwrap();
         assert_eq!(rx2.recv().unwrap(), 1);
+    }
+
+    #[test]
+    fn relative_mode() {
+        let (tx1, rx1) = crossbeam_channel::unbounded();
+        let (tx2, rx2) = crossbeam_channel::unbounded();
+        let mut c = Computer::new(vec![109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99], rx1, tx2);
+        c.run_to_completion().unwrap();
+        assert_eq!(rx2.recv().unwrap(), 109);
+    }
+
+    #[test]
+    fn large_num() {
+        let (tx1, rx1) = crossbeam_channel::unbounded();
+        let (tx2, rx2) = crossbeam_channel::unbounded();
+        let mut c = Computer::new(vec![104, 1125899906842624, 99], rx1, tx2);
+        c.run_to_completion().unwrap();
+        assert_eq!(rx2.recv().unwrap(), 1125899906842624);
+    }
+
+    #[test]
+    fn large_calc() {
+        let (tx1, rx1) = crossbeam_channel::unbounded();
+        let (tx2, rx2) = crossbeam_channel::unbounded();
+        let mut c = Computer::new(vec![1102, 34915192, 34915192, 7, 4, 7, 99, 0], rx1, tx2);
+        c.run_to_completion().unwrap();
+        assert_eq!(rx2.recv().unwrap(), 1219070632396864);
     }
 }
---
+++ src/day9/main.rs
@@ -0,0 +1,28 @@
+use std::boxed::Box;
+use std::result::Result;
+use std::io::BufRead;
+use std::error::Error;
+use crossbeam_channel::{Sender, Receiver};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    let stdin = std::io::stdin();
+    let mut stdin_locked = stdin.lock();
+    stdin_locked.read_line(&mut input)?;
+    let memory: Vec<i64> =
+        input.split(',')
+            .map(|i| i.trim().parse().unwrap())
+            .collect();
+    let mut stdout = std::io::stdout();
+
+    let (tx1, rx1) = crossbeam_channel::unbounded();
+    let (tx2, rx2) = crossbeam_channel::unbounded();
+    tx1.send(1);
+    let mut computer = aoc::computer::Computer::new(memory, rx1, tx2);
+    computer.run_to_completion()?;
+    loop {
+        let val = rx2.recv()?;
+        println!("{}", val);
+    }
+    Ok(())
+}
