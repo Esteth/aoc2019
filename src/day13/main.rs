aa97e4c4ef9d342236193034d86c022610c00d3c
esteth <adam.copp@gmail.com>
2019-12-13 09:19:38 +0000

    Solve 14-1. Ugly. Mostly re-used the turtle code. Anticipating a part 2 where I simulate the game.

--- src/day13/main.rs
+++ src/day13/main.rs
@@ -1,6 +1,77 @@
 use std::error::Error;
+use std::io::BufRead;
+use std::collections::{HashMap, HashSet};
+use std::thread;
+use crossbeam_channel::RecvError;
+use simple_error::SimpleError;
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone)]
+enum Pixel {
+    Empty,
+    Wall,
+    Block,
+    Paddle,
+    Ball,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, Day 13");
+    let mut input = String::new();
+    let stdin = std::io::stdin();
+    let mut stdin_locked = stdin.lock();
+    stdin_locked.read_line(&mut input)?;
+    let memory: Vec<i64> =
+        input.split(',')
+            .map(|i| i.trim().parse().unwrap())
+            .collect();
+
+    let mut panels: HashMap<(i32, i32), bool> = HashMap::new();
+
+    let (input_sender, input_receiver) = crossbeam_channel::unbounded();
+    let (output_sender, output_receiver) = crossbeam_channel::unbounded();
+    let (input_request_sender, input_request_receiver) = crossbeam_channel::unbounded();
+    thread::spawn(move || {
+        let mut computer = aoc::computer::Computer::new(memory, input_receiver, input_request_sender, output_sender);
+        computer.run_to_completion();
+    });
+
+    let mut pixels: HashMap<(i64, i64), Pixel> = HashMap::new();
+    loop {
+        let x = match output_receiver.recv() {
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        let y = output_receiver.recv()?;
+        let pixel = match output_receiver.recv() {
+            Ok(0) => Pixel::Empty,
+            Ok(1) => Pixel::Wall,
+            Ok(2) => Pixel::Block,
+            Ok(3) => Pixel::Paddle,
+            Ok(4) => Pixel::Ball,
+            _ => break,
+        };
+        pixels.insert((x, y), pixel);
+    }
+
+    let mut blocks_count = pixels.iter()
+        .filter(|(p, pixel)| **pixel == Pixel::Block)
+        .count();
+    println!("number of blocks: {}", blocks_count);
+//        .map(|(p, _)| *p)
+//        .collect();
+//    let min_x = white_panels.iter().min_by_key(|(x, _)| *x).unwrap().0;
+//    let max_x = white_panels.iter().max_by_key(|(x, _)| *x).unwrap().0;
+//    let min_y = white_panels.iter().min_by_key(|(_, y)| *y).unwrap().1;
+//    let max_y = white_panels.iter().max_by_key(|(_, y)| *y).unwrap().1;
+//
+//    for y in (min_y..=max_y).rev() {
+//        for x in min_x..=max_x {
+//            if white_panels.contains(&(x, y)) {
+//                print!("█");
+//            } else {
+//                print!(" ");
+//            }
+//        }
+//        print!("\n");
+//    }
     Ok(())
 }
